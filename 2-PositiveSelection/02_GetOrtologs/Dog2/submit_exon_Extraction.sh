9365cdcde0bc230bb2400f4de89f860712b523d1
dechavezv <dechavezv@ucla.edu>
2020-01-20 17:32:52 -0800

    changing_path_of_directory

--- 2-PositiveSelection/02_GetOrtologs/Dog2/submit_exon_Extraction.sh
+++ 2-PositiveSelection/02_GetOrtologs/Dog2/submit_exon_Extraction.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
-#$ -wd /u/home/d/dechavez/project-rwayne/Dog2
+#$ -wd /u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/02_GetOrtologs/Dog2
 #$ -l highp,h_rt=02:00:00,h_data=1G
 #$ -N ortolog
-#$ -o /u/home/d/dechavez/project-rwayne/Dog2/log/MkrIlAdp
-#$ -e /u/home/d/dechavez/project-rwayne/Dog2/log/MkrIlAdp
+#$ -o /u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/02_GetOrtologs/Dog2/log/MkrIlAdp
+#$ -e /u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/02_GetOrtologs/Dog2/log/MkrIlAdp
 #$ -m abe
 #$ -M dechavezv
 
 
-SCRIPTDIR=/u/home/d/dechavez/project-rwayne/Dog2
+SCRIPTDIR=/u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/02_GetOrtologs/Dog2
 
 QSUB=/u/systems/UGE8.6.4/bin/lx-amd64/qsub
 
 # Important change the pathway to your current path
-cd /u/home/d/dechavez/project-rwayne/Dog2
+cd /u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/02_GetOrtologs/Dog2
 
 # chan the number 2 for the name you chose for your Dog folder (eg. Dog4 will be Extract_Exons_3.sh ${i} 4).
 # See the readme.txt for more details
