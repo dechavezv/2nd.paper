766c595b24379f837e1b35a093aa7c88c6835ea7
dechavezv <dechavezv@ucla.edu>
2020-01-27 16:31:01 -0800

    fixing.bugs

--- 2-PositiveSelection/04-Run_SWAMP/scripts/submit_SWMP.partII.sh
+++ 2-PositiveSelection/04-Run_SWAMP/scripts/submit_SWMP.partII.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
+
 #$ -wd /u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/04-Run_SWAMP
-#$ -l highp,h_rt=36:00:00,h_data=1G
+#$ -l highp,h_rt=26:00:00,h_data=1G
 #$ -N II_SWAMP
 #$ -o /u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/04-Run_SWAMP/log/SWAMP_II
-#$ -e /u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/04-Run_SWAMP//log/SWAMP_II
+#$ -e /u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/04-Run_SWAMP/log/SWAMP_II
 #$ -m abe
 #$ -M dechavezv
 
-SCRIPTDIR=/u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/04-Run_SWAMP/scripts
-DIREC=/u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/04-Run_SWAMP
+export SCRIPTDIR=/u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/04-Run_SWAMP/scripts
+export DIREC=/u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/04-Run_SWAMP
 
 QSUB=/u/systems/UGE8.6.4/bin/lx-amd64/qsub
 
-cd ${DIREC}
-for file in dir*; do (echo $dir && cd $dir && $QSUB ${SCRIPTDIR}/SWAMP.sh);done > Paml.on.swamp.log 
+cd ${DIREC}/processing
+
+for dir in dir*; do (echo $dir && cd $dir && $QSUB ${SCRIPTDIR}/SWAMP.sh);done
