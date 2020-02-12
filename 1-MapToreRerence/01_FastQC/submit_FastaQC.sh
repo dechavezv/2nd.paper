4633039595cd8822154038608ec3c09e92556041
dechavezv <dechavezv@ucla.edu>
2020-02-11 16:42:32 -0800

    be consisten and call variables with export

--- 1-MapToreRerence/01_FastQC/submit_FastaQC.sh
+++ 1-MapToreRerence/01_FastQC/submit_FastaQC.sh
@@ -13,8 +13,9 @@
 #usage [dir] [read_1] [read_2] [outfile] [RG] [sample] [library] [flowcell] [seq center]
 
 QSUB=/u/systems/UGE8.6.4/bin/lx-amd64/qsub
-SCRIPT=/u/home/d/dechavez/project-rwayne/2nd.paper/1-MapToreRerence/01_FastQC/run_FastaQC.sh
+export SCRIPT=/u/home/d/dechavez/project-rwayne/2nd.paper/1-MapToreRerence/01_FastQC/run_FastaQC.sh
+export DIR=/u/home/d/dechavez/project-rwayne/QB3.SA.WolfHeav
 
-DIR=/u/scratch/d/dechavez/BD
+cd ${DIR}
 
 ${QSUB} ${SCRIPT} ${DIR} Bush-dog_R0902_2_1.fastq.gz Bush-dog_R0902_2_2.fastq.gz BD.bam BD_1a BD Lib1 C7424ACXX Smithsonian
