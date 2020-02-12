36b424ae1527371eb73c259bcc42e1a97f37e958
dechavezv <dechavezv@ucla.edu>
2020-02-11 16:38:04 -0800

    better if provide the whole input name

--- 1-MapToreRerence/01_FastQC/FastaQC.sh
+++ 1-MapToreRerence/01_FastQC/FastaQC.sh
@@ -3,18 +3,18 @@
 #Use highmem te reserve a whole node
 
 #$ -l highp,h_rt=22:00:00,h_data=5G
-#$ -N FASTAQC_EurAsi.jackal
+#$ -N FASTAQC_Lvet
 #$ -cwd
 #$ -m bea
-#$ -o /u/scratch/d/dechavez/Jackal/log/FASTAQC.out
-#$ -e /u/scratch/d/dechavez/Jackal/log/FASTAQC.err
+#$ -o /u/home/d/dechavez/project-rwayne/QB3.SA.WolfHeav/log/FASTAQC.out
+#$ -e /u/home/d/dechavez/project-rwayne/QB3.SA.WolfHeav/log/FASTAQC.err
 #$ -M dechavezv
 
 # then load your modules:
 . /u/local/Modules/default/init/modules.sh
 module load java
 
-export RAW_read=/u/home/d/dechavez/project-rwayne/Jackal
+export RAW_read=/u/scratch/d/dechavez/Lvet
 export FASTAQC=/u/home/d/dechavez/project-rwayne/FastQC
 
 echo "########"
@@ -24,14 +24,13 @@ echo "#######"
 FastaQC_fn () {
 	echo "***** Beginning FastaqC of $1 *****"
 
- 	${FASTAQC}/fastqc ${RAW_read}/${1}_1.fastq.gz 
-	${FASTAQC}/fastqc ${RAW_read}/${1}_2.fastq.gz 
+ 	${FASTAQC}/fastqc ${RAW_read}/${1} 
+	${FASTAQC}/fastqc ${RAW_read}/${1}
 
         echo "***** $1 processing complete *****"
 }
 
-FastaQC_fn SRR8049192 
+FastaQC_fn $1
 
-#FastaQC_fn CL100060195_L02_S2
 
-#FastaQC_fn LS24_S7
+
