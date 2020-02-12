e6081dd1ff2ce43a49d36a600afaf3a795424d58
dechavezv <dechavezv@ucla.edu>
2020-02-12 12:03:25 -0800

    this should work for the fox

--- 1-MapToreRerence/01_FastQC/submit_FastaQC.sh
+++ 1-MapToreRerence/01_FastQC/submit_FastaQC.sh
@@ -1,25 +1,30 @@
 #! /bin/bash
 
-#$ -wd /u/scratch/d/dechavez/BD
+#$ -wd /u/home/d/dechavez/project-rwayne/Lvet/raw.reads
 #$ -l highp,h_rt=02:00:00,h_data=1G
-#$ -N subfq2sam
-#$ -o /u/scratch/d/dechavez/BD/log/FastqToSam
-#$ -e /u/scratch/d/dechavez/BD/log/FastqToSam
+#$ -N subFastqc
+#$ -o /u/home/d/dechavez/project-rwayne/Lvet/raw.reads/log/FastqQC
+#$ -e /u/home/d/dechavez/project-rwayne/Lvet/raw.reads/log/FastqQC
 #$ -m abe
 #$ -M dechavezv
 
 #highmem
 
-#usage [dir] [read_1] [read_2] [outfile] [RG] [sample] [library] [flowcell] [seq center]
+#usage [script] [sampleName.Read]
+
+
+# load your modules:
+. /u/local/Modules/default/init/modules.sh
+module load perl
 
 QSUB=/u/systems/UGE8.6.4/bin/lx-amd64/qsub
 export SCRIPT=/u/home/d/dechavez/project-rwayne/2nd.paper/1-MapToreRerence/01_FastQC/run_FastaQC.sh
-export DIR=/u/home/d/dechavez/project-rwayne/QB3.SA.WolfHeav
+export DIR=/u/home/d/dechavez/project-rwayne/Lvet/raw.reads
 
 cd ${DIR}
 
-#save name of samples withouth the _R1_001.fastq.gz  or _R2_001.fastq.gz extension  
-ls *.fastq.gz | perl -pe 's/_R\d+_\d+.fastq.gz//g' | uniq > list.of.samples.txt
+#save name of samples withouth the _R1_001.fastq.gz  or _R2_001.fastq.gz extension 
+ls *.fastq.gz | perl -pe 's/_R\d+.fastq.gz//g' | uniq > list.of.samples.txt
 
 # then itarate through the list and run Fastaqc
 for line in $(cat list.of.samples.txt); do ${QSUB} ${SCRIPT} $line;done
