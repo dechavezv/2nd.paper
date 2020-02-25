32edb8255567545a3a58218ddd7818b26ce768ed
dechavezv <dechavezv@ucla.edu>
2020-02-24 19:22:16 -0800

    adding one sample

--- 1-MapToreRerence/09_Genotype_gVCFs/Bychr/submit_GATK_GenoGVCFs_MW.sh
+++ 1-MapToreRerence/09_Genotype_gVCFs/Bychr/submit_GATK_GenoGVCFs_MW.sh
@@ -2,7 +2,7 @@
 
 #$ -wd /u/home/d/dechavez/project-rwayne/MW/VCF
 #$ -l h_rt=20:00:00,h_data=21G,highp
-#$ -t 23-26:1
+#$ -t 1-38:1
 #$ -N GTgVCF
 #$ -o /u/home/d/dechavez/project-rwayne/MW/VCF/log/reports
 #$ -e /u/home/d/dechavez/project-rwayne/MW/VCF/log/reports
@@ -17,5 +17,5 @@ java -jar -Xmx16g /u/local/apps/gatk/3.7/GenomeAnalysisTK.jar \
 -R /u/home/d/dechavez/project-rwayne/canfam31/canfam31.fa \
 -allSites \
 -L chr$(printf "%02d" "$SGE_TASK_ID") \
-$(for j in {01..4}; do echo "-V /u/scratch/d/dechavez/MW/GVCFs/bcbr${j}_chr$(printf "%02d" "$SGE_TASK_ID").g.vcf.gz "; done) \
+$(for j in {01..5}; do echo "-V /u/scratch/d/dechavez/MW/GVCFs/bcbr${j}_chr$(printf "%02d" "$SGE_TASK_ID").g.vcf.gz "; done) \
 -o /u/home/d/dechavez/project-rwayne/MW/VCF/bcbr_joint_chr$(printf "%02d" "$SGE_TASK_ID").vcf.gz
