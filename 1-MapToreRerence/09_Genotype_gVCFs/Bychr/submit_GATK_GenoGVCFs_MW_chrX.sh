c65febc02c84b1caa6976b5d8477c94673af3443
dechavezv <dechavezv@ucla.edu>
2020-02-24 19:24:29 -0800

    adding one sample

---
+++ 1-MapToreRerence/09_Genotype_gVCFs/Bychr/submit_GATK_GenoGVCFs_MW_chrX.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#$ -wd /u/home/d/dechavez/project-rwayne/MW/VCF
+#$ -l h_rt=20:00:00,h_data=21G,highp
+#$ -N GTgVCF
+#$ -o /u/home/d/dechavez/project-rwayne/MW/VCF/log/reports
+#$ -e /u/home/d/dechavez/project-rwayne/MW/VCF/log/reports
+#$ -m abe
+#$ -M dechavezv
+
+source /u/local/Modules/default/init/modules.sh
+module load java
+
+java -jar -Xmx16g /u/local/apps/gatk/3.7/GenomeAnalysisTK.jar \
+-T GenotypeGVCFs \
+-R /u/home/d/dechavez/project-rwayne/canfam31/canfam31.fa \
+-allSites \
+-stand_emit_conf 0 \
+-stand_call_conf 0 \
+-L chrX \
+$(for j in {01..5}; do echo "-V /u/scratch/d/dechavez/MW/GVCFs/bcbr${j}_chrX.g.vcf.gz "; done) \
+-o /u/home/d/dechavez/project-rwayne/MW/VCF/MW_allSamples_chrX.vcf.gz
