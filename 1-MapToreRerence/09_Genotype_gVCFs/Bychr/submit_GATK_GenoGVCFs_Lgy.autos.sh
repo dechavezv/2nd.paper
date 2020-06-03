3e79c82112edbc42f04614c1c8e80fea54334e66
dechavezv <dechavezv@ucla.edu>
2020-06-03 10:32:53 -0700

    running just chr1

---
+++ 1-MapToreRerence/09_Genotype_gVCFs/Bychr/submit_GATK_GenoGVCFs_Lgy.autos.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+#$ -wd /u/home/d/dechavez/project-rwayne/Lvet/Lgy/GVCFs
+#$ -l highp,h_data=8G,h_rt=33:00:00,h_vmem=30G
+#$ -t 1-1:1
+#$ -N GTgVCF
+#$ -o /u/home/d/dechavez/project-rwayne/Lvet/Lgy/GVCFs/log/
+#$ -e /u/home/d/dechavez/project-rwayne/Lvet/Lgy/GVCFs/log/
+#$ -m abe
+#$ -M dechavezv
+
+source /u/local/Modules/default/init/modules.sh
+module load java
+
+java -jar -Xmx8g /u/local/apps/gatk/3.7/GenomeAnalysisTK.jar \
+-T GenotypeGVCFs \
+-R /u/home/d/dechavez/project-rwayne/canfam31/canfam31.fa \
+-allSites \
+-L chr$(printf "%02d" "$SGE_TASK_ID") \
+$(echo "-V /u/home/d/dechavez/project-rwayne/Lvet/Lgy/GVCFs/Lgy01_chr$(printf "%02d" "$SGE_TASK_ID").g.vcf.gz") \
+-o /u/home/d/dechavez/project-rwayne/Lvet/Lgy/VCF/Lgy01_chr$(printf "%02d" "$SGE_TASK_ID").vcf.gz
