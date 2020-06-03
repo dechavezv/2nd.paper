e121a5497c98812374a204acd3361da682b440d3
dechavezv <dechavezv@ucla.edu>
2020-06-03 10:04:51 -0700

    make it work on any sample but have to provide input

--- 1-MapToreRerence/09_Genotype_gVCFs/run_GATK_TrimAlt_AddAnnot_MW_X.sh
+++ 1-MapToreRerence/09_Genotype_gVCFs/run_GATK_TrimAlt_AddAnnot_MW_X.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
-#$ -wd /u/home/d/dechavez/project-rwayne/MW/VCF
+#$ -wd /u/scratch/d/dechavez/SA.VCF 
 #$ -l h_rt=24:00:00,h_data=4G,highp,h_vmem=20G
-#$ -q *@n7159
-#$ -N n7159trim_annot
-#$ -o /u/home/d/dechavez/project-rwayne/MW/VCF
-#$ -e /u/home/d/dechavez/project-rwayne/MW/VCF
+#$ -N trim_annot
+#$ -o /u/scratch/d/dechavez/SA.VCF/log
+#$ -e /u/scratch/d/dechavez/SA.VCF/log
 #$ -m abe
 #$ -M dechavezv
 
@@ -16,16 +15,17 @@ module load java
 
 GATK=/u/local/apps/gatk/3.7/GenomeAnalysisTK.jar
 REFERENCE=/u/home/d/dechavez/project-rwayne/canfam31/canfam31.fa
+PREFIX=$1
 
 cd /u/home/d/dechavez/project-rwayne/MW/VCF
 
-java -jar -Xmx3g ${GATK} \
+java -jar -Xmx4g ${GATK} \
 -T SelectVariants \
 -R ${REFERENCE} \
 -L chrX \
 -trimAlternates \
--V bcbr_joint_chrX.vcf.gz \
--o bcbr_joint_chrX_TrimAlt.vcf.gz
+-V ${PREFIX}_chrX.vcf.gz \
+-o ${PREFIX}_chrX_TrimAlt.vcf.gz
 
 java -jar -Xmx3g ${GATK} \
 -T VariantAnnotator \
@@ -34,5 +34,5 @@ java -jar -Xmx3g ${GATK} \
 -A VariantType \
 -A AlleleBalance \
 -L chrX \
--V bcbr_joint_chrX_TrimAlt.vcf.gz \
--o bcbr_joint_chrX_TrimAlt_Annot.vcf.gz
+-V ${PREFIX}_chrX_TrimAlt.vcf.gz \
+-o ${PREFIX}_chrX_TrimAlt_Annot.vcf.gz
