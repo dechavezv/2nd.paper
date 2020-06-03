d2a7172bb6db061f0eed4f010893815e0fb4e86d
dechavezv <dechavezv@ucla.edu>
2020-06-03 14:02:00 -0700

    1st step get run plink format

---
+++ 4-Demography/ROH/Step1_ROH_VCFtoPLINK.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+#$ -cwd
+#$ -l h_rt=04:00:00,h_data=8G,highp,h_vmem=30G,highmem_forced=TRUE
+#$ -N plinkROH
+#$ -o /u/scratch/d/dechavez/SA.VCF/log/
+#$ -e /u/scratch/d/dechavez/SA.VCF/log/
+#$ -m abe
+#$ -M dechavezv
+#$ -t 1-38
+
+# highmem 
+
+# vcftools LROH requires >1 individual
+# plink doesn't
+
+source /u/local/Modules/default/init/modules.sh
+module load vcftools
+module load plink
+
+# need to get chr name from file
+i=$(printf "%02d" "$SGE_TASK_ID")
+#i=39
+Sample=$1
+wd=/u/scratch/d/dechavez/SA.VCF/Filtered/20200530
+vcf=/u/scratch/d/dechavez/BD/ROH/${Sample%.txt}_chr${i}_TrimAlt_Annot_Mask_Filter_passingSNPs.vcf.gz
+
+# convert to ped/map format. note that you lose chromosome info - that's a pain.
+plinkindir=$wd/plinkInputFiles
+plinkoutdir=$wd/plinkOutputFiles
+mkdir -p $plinkindir
+vcftools --gzvcf $vcf --plink --chr chr$i --out $plinkindir/${Sample%.txt}.${i}.HQsites.Only.rmDotGenotypes.rmBadVars.Plink
