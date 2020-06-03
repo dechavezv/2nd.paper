3555b49319bc3ae83640bb40998620a9df2f0616
dechavezv <dechavezv@ucla.edu>
2020-06-03 11:39:45 -0700

    filteringAndeanF

--- 1-MapToreRerence/06_RemoveBadReads/submit_run_RemoveBadReads_Lvet.sh
+++ 1-MapToreRerence/06_RemoveBadReads/submit_run_RemoveBadReads_Lvet.sh
@@ -3,8 +3,8 @@
 #$ -wd /u/scratch/d/dechavez/IndelReal
 #$ -l highp,h_rt=10:00:00,h_data=1G
 #$ -N subbamfltr
-#$ -o /u/scratch/d/dechavez/IndelReal/log/bamfilter.out
-#$ -e /u/scratch/d/dechavez/IndelReal/log/bamfilter.err
+#$ -o /u/scratch/d/dechavez/IndelReal/log/
+#$ -e /u/scratch/d/dechavez/IndelReal/log/
 #$ -m abe
 #$ -M dechavezv
 
@@ -13,7 +13,7 @@ QSUB=/u/systems/UGE8.6.4/bin/lx-amd64/qsub
 
 cd /u/scratch/d/dechavez/IndelReal
 
-${QSUB} -N bamfiltr $SCRIPT_DIR/run_RemoveBadReads_Lvet.sh BBJ_BWA_sortRG_rmdup_realign_fixmate.bam
+${QSUB} -N bamfiltr $SCRIPT_DIR/run_RemoveBadReads_Lvet.sh Andean_Fox_merged_sortRG_MarkPCRDup_realign_fixmate.bam
 
 #for file in *MarkDup_Filtered.bam; do
 ## ${QSUB} -N bamfiltr $SCRIPT_DIR/run_RemoveBadReads_Lvet.sh ${file}
