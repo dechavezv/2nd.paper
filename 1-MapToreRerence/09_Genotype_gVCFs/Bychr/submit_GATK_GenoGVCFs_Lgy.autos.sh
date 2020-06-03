a8b325ca753143c32ab1c9e630b03920e14f5469
dechavezv <dechavezv@ucla.edu>
2020-06-03 10:34:28 -0700

    running just chr22 add more mem

--- 1-MapToreRerence/09_Genotype_gVCFs/Bychr/submit_GATK_GenoGVCFs_Lgy.autos.sh
+++ 1-MapToreRerence/09_Genotype_gVCFs/Bychr/submit_GATK_GenoGVCFs_Lgy.autos.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 #$ -wd /u/home/d/dechavez/project-rwayne/Lvet/Lgy/GVCFs
-#$ -l highp,h_data=8G,h_rt=33:00:00,h_vmem=30G
-#$ -t 1-1:1
+#$ -l highp,h_data=10G,h_rt=33:00:00,h_vmem=30G
+#$ -t 22-22:1
 #$ -N GTgVCF
 #$ -o /u/home/d/dechavez/project-rwayne/Lvet/Lgy/GVCFs/log/
 #$ -e /u/home/d/dechavez/project-rwayne/Lvet/Lgy/GVCFs/log/
@@ -12,7 +12,7 @@
 source /u/local/Modules/default/init/modules.sh
 module load java
 
-java -jar -Xmx8g /u/local/apps/gatk/3.7/GenomeAnalysisTK.jar \
+java -jar -Xmx10g /u/local/apps/gatk/3.7/GenomeAnalysisTK.jar \
 -T GenotypeGVCFs \
 -R /u/home/d/dechavez/project-rwayne/canfam31/canfam31.fa \
 -allSites \
