2dbaf681a0c424333ce6669ae048e1eacf3d707c
dechavezv <dechavezv@ucla.edu>
2020-06-04 10:13:06 -0700

    arrays not working hoffman

--- 1-MapToreRerence/09_Genotype_gVCFs/run_GATK_TrimAlt_AddAnnot_MW_autos.sh
+++ 1-MapToreRerence/09_Genotype_gVCFs/run_GATK_TrimAlt_AddAnnot_MW_autos.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
+
 #$ -wd /u/home/d/dechavez/project-rwayne/MW/VCF
-#$ -l highmem,highp,h_rt=24:00:00,h_data=10G,h_vmem=30G,arch=intel*
+#$ -l highmem,highp,h_rt=24:00:00,h_data=10G,h_vmem=40G,arch=intel*
 #$ -N trim_annot
 #$ -o /u/home/d/dechavez/project-rwayne/MW/VCF
 #$ -e /u/home/d/dechavez/project-rwayne/MW/VCF
 #$ -m abe
 #$ -M dechavezv
-#$ -t 23-23:1
+### #$ -t 2-2:1
 
 #highmem
 
@@ -17,14 +18,15 @@ GATK=/u/local/apps/gatk/3.7/GenomeAnalysisTK.jar
 REFERENCE=/u/home/d/dechavez/project-rwayne/canfam31/canfam31.fa
 
 cd /u/home/d/dechavez/project-rwayne/MW/VCF
+chr=$1
 
 java -jar -Xmx10g ${GATK} \
 -T SelectVariants \
 -R ${REFERENCE} \
 -L chr$(printf "%02d" "$SGE_TASK_ID") \
 -trimAlternates \
--V bcbr05_chr$(printf "%02d" "$SGE_TASK_ID").vcf.gz \
--o bcbr05_chr$(printf "%02d" "$SGE_TASK_ID")_TrimAlt.vcf.gz
+-V bcbr05_chr${chr}.vcf.gz \
+-o bcbr05_chr${chr}_TrimAlt.vcf.gz
 
 java -jar -Xmx10g ${GATK} \
 -T VariantAnnotator \
@@ -33,5 +35,5 @@ java -jar -Xmx10g ${GATK} \
 -A VariantType \
 -A AlleleBalance \
 -L chr$(printf %02d $SGE_TASK_ID) \
--V bcbr05_chr$(printf "%02d" "$SGE_TASK_ID")_TrimAlt.vcf.gz \
--o bcbr05_chr$(printf "%02d" "$SGE_TASK_ID")_TrimAlt_Annot.vcf.gz 
+-V bcbr05_chr${chr}_TrimAlt.vcf.gz \
+-o bcbr05_chr${chr}_TrimAlt_Annot.vcf.gz 
