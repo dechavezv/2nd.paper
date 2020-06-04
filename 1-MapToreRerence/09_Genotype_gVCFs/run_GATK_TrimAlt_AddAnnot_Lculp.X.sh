f3f95fe39f10c05d10fd11f756d9cff7e3a6c5b8
dechavezv <dechavezv@ucla.edu>
2020-06-04 16:57:26 -0700

    trimAnot Lculp

---
+++ 1-MapToreRerence/09_Genotype_gVCFs/run_GATK_TrimAlt_AddAnnot_Lculp.X.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+#$ -wd /u/home/d/dechavez/project-rwayne/Lculp/GVCFs
+#$ -l highp,h_vmem=34G,h_rt=24:00:00,h_data=7G,arch=intel*
+#$ -N LculpTrimAnnotX
+#$ -o /u/home/d/dechavez/project-rwayne/Lculp/log/
+#$ -e /u/home/d/dechavez/project-rwayne/Lculp/log/
+#$ -m abe
+#$ -M dechavezv
+
+#highmem,highp
+
+source /u/local/Modules/default/init/modules.sh
+module load java
+
+GATK=/u/local/apps/gatk/3.7/GenomeAnalysisTK.jar
+REFERENCE=/u/home/d/dechavez/project-rwayne/canfam31/canfam31.fa
+
+cd /u/home/d/dechavez/project-rwayne/Lculp/GVCFs
+
+java -jar -Xmx7g ${GATK} \
+-T SelectVariants \
+-R ${REFERENCE} \
+-L chrX \
+-trimAlternates \
+-V Lculp01_chrX.vcf.gz \
+-o Lculp01_chrX_TrimAlt.vcf.gz
+
+java -jar -Xmx7g ${GATK} \
+-T VariantAnnotator \
+-R ${REFERENCE} \
+-G StandardAnnotation \
+-A VariantType \
+-A AlleleBalance \
+-L chr$(printf %02d $SGE_TASK_ID) \
+-V Lculp01_chrX_TrimAlt.vcf.gz \
+-o Lculp01_chrX_TrimAlt_Annot.vcf.gz 
