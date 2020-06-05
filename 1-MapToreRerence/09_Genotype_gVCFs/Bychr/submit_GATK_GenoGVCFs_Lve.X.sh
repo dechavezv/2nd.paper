3c161ad8c84aa20f1bec0530c1a6c24555f25489
dechavezv <dechavezv@ucla.edu>
2020-06-05 16:24:12 -0700

    now script is running after qsub

--- 1-MapToreRerence/09_Genotype_gVCFs/Bychr/submit_GATK_GenoGVCFs_Lve.X.sh
+++ 1-MapToreRerence/09_Genotype_gVCFs/Bychr/submit_GATK_GenoGVCFs_Lve.X.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
-#$ -wd /u/home/d/dechavez/project-rwayne/Lvet/Lve/GVCFs
-#$ -l highp,h_data=10G,h_rt=24:00:00,h_vmem=30G,arch=inter*
+#$ -wd /u/home/d/dechavez/project-rwayne/Lvet/Lvet/GVCFs
+#$ -l highp,h_data=20G,h_rt=24:00:00,h_vmem=30G,arch=intel*
 #$ -N GTgVCF
-#$ -o /u/home/d/dechavez/project-rwayne/Lvet/Lve/GVCFs/log/
-#$ -e /u/home/d/dechavez/project-rwayne/Lvet/Lve/GVCFs/log/
+#$ -o /u/home/d/dechavez/project-rwayne/Lvet/Lvet/log/
+#$ -e /u/home/d/dechavez/project-rwayne/Lvet/Lvet/log/
 #$ -m abe
 #$ -M dechavezv
 
 source /u/local/Modules/default/init/modules.sh
 module load java
 
-java -jar -Xmx10g /u/local/apps/gatk/3.7/GenomeAnalysisTK.jar \
+java -jar -Xmx20g /u/local/apps/gatk/3.7/GenomeAnalysisTK.jar \
 -T GenotypeGVCFs \
 -R /u/home/d/dechavez/project-rwayne/canfam31/canfam31.fa \
 -allSites \
