57e95916c3f31b94568d451a90df206ea900bd0a
dechavezv <dechavezv@ucla.edu>
2020-06-07 15:59:18 -0700

    missing Lculp

--- 4-Demography/ROH/submit_ROH_Step1-2.sh
+++ 4-Demography/ROH/submit_ROH_Step1-2.sh
@@ -7,7 +7,7 @@
 #$ -e /u/scratch/d/dechavez/SA.VCF/log/
 #$ -m abe
 #$ -M dechavezv
-#$ -t 1-10:1
+### #$ -t 1-10:1
 
 SCRIPT_DIR=/u/home/d/dechavez/project-rwayne/2nd.paper/4-Demography/ROH
 QSUB=/u/systems/UGE8.6.4/bin/lx-amd64/qsub
@@ -20,7 +20,9 @@ cd /u/scratch/d/dechavez/SA.VCF/Filtered/20200530
 ## ${QSUB} -N ROHstep1 $SCRIPT_DIR/Step1_ROH_VCFtoPLINK.sh ${line}
 #done
 
-#sleep 25m
+${QSUB} -N ROHstep1 $SCRIPT_DIR/Step1_ROH_VCFtoPLINK.sh Lculp
+
+sleep 35m
 
 cd plinkInputFiles
 
@@ -34,10 +36,9 @@ cd plinkInputFiles
 ## do for g in .02 .05 .1; \
 ## do echo $a $b $c $d $e $f $g >> temp.txt ; done; done; done; done; done; done; done
 
+${QSUB} -N ROHstep2 ${SCRIPT_DIR}/Step2_run_PLINK_plot.sh Lculp $(head -n ${SGE_TASK_ID} temp.txt | tail -n 1)
+
 ## for line in $(cat ${SCRIPT_DIR}/list.sp.ROH.Het.txt); do
 ### ${QSUB} -N ROHstep2 ${SCRIPT_DIR}/Step2_run_PLINK_plot.sh ${line} $(head -n ${SGE_TASK_ID} temp.txt | tail -n 1)
 ## done
-
-#teting can provide any sample for now
-${QSUB} -N ROHstep2 ${SCRIPT_DIR}/Step2_run_PLINK_plot.sh Sve338 50 50 1000 50 1 1 .02
 
