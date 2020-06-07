b4a4e90591a7e454ee8dbe40879d353e22754bf7
dechavezv <dechavezv@ucla.edu>
2020-06-07 11:00:59 -0700

    try the 10 first parameters for now

--- 4-Demography/ROH/submit_ROH_Step1-2.sh
+++ 4-Demography/ROH/submit_ROH_Step1-2.sh
@@ -7,7 +7,7 @@
 #$ -e /u/scratch/d/dechavez/SA.VCF/log/
 #$ -m abe
 #$ -M dechavezv
-### #$ -t 1-10:1
+#$ -t 1-10:1
 
 SCRIPT_DIR=/u/home/d/dechavez/project-rwayne/2nd.paper/4-Demography/ROH
 QSUB=/u/systems/UGE8.6.4/bin/lx-amd64/qsub
@@ -36,7 +36,9 @@ do for f in 1 2 3 4 5 10 20; \
 do for g in .02 .05 .1; \
 do echo $a $b $c $d $e $f $g >> temp.txt ; done; done; done; done; done; done; done
 
-### ${QSUB} -N ROHstep2 ${SCRIPT_DIR}/Step2_run_PLINK_plot.sh $(head -n ${SGE_TASK_ID} temp.txt | tail -n 1)
+for line in $(cat ${SCRIPT_DIR}/list.sp.ROH.Het.txt); do
+${QSUB} -N ROHstep2 ${SCRIPT_DIR}/Step2_run_PLINK_plot.sh ${line} $(head -n ${SGE_TASK_ID} temp.txt | tail -n 1)
+done
 
 
 #sleep 6h
