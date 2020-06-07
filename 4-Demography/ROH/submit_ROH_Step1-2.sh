c3563dc6874fbb6beea83fc8729703a5df032fc1
dechavezv <dechavezv@ucla.edu>
2020-06-07 09:42:42 -0700

    provide name of list sps and fix directy name

--- 4-Demography/ROH/submit_ROH_Step1-2.sh
+++ 4-Demography/ROH/submit_ROH_Step1-2.sh
@@ -7,20 +7,20 @@
 #$ -e /u/scratch/d/dechavez/SA.VCF/log/
 #$ -m abe
 #$ -M dechavezv
-#$ -t 1-10:1
+### #$ -t 1-10:1
 
 SCRIPT_DIR=/u/home/d/dechavez/project-rwayne/2nd.paper/4-Demography/ROH
 QSUB=/u/systems/UGE8.6.4/bin/lx-amd64/qsub
 
-cd /u/scratch/d/dechavez/SA.VCF/Filtered/20200530s
+cd /u/scratch/d/dechavez/SA.VCF/Filtered/20200530
 
 #Prive a list with name of samples
 
-for line in $(cat list.sp.ROH.Het.txt); do
-${QSUB} -N ROHstep1 $SCRIPT_DIR/Step1_ROH_VCFtoPLINK.sh $line
+for line in $(cat ${SCRIPT_DIR}/list.sp.ROH.Het.txt); do
+${QSUB} -N ROHstep1 $SCRIPT_DIR/Step1_ROH_VCFtoPLINK.sh ${line}
 done
 
-sleep 45m
+sleep 25m
 
 cd plinkInputFiles
 
@@ -36,7 +36,7 @@ do for f in 1 2 3 4 5 10 20; \
 do for g in .02 .05 .1; \
 do echo $a $b $c $d $e $f $g >> temp.txt ; done; done; done; done; done; done; done
 
-${QSUB} -N ROHstep2 ${SCRIPT_DIR}/Step2_run_PLINK_plot.sh $(head -n ${SGE_TASK_ID} temp.txt | tail -n 1)
+### ${QSUB} -N ROHstep2 ${SCRIPT_DIR}/Step2_run_PLINK_plot.sh $(head -n ${SGE_TASK_ID} temp.txt | tail -n 1)
 
 
 #sleep 6h
