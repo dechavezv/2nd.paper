b4c2060e2fdac0eb054350bed73e5bd3020140ca
dechavezv <dechavezv@ucla.edu>
2020-06-07 15:43:24 -0700

    runing Lculp that was missing

--- 4-Demography/PCA/submit_run_get_polymorphic_SA.canids.sh
+++ 4-Demography/PCA/submit_run_get_polymorphic_SA.canids.sh
@@ -13,8 +13,8 @@ QSUB=/u/systems/UGE8.6.4/bin/lx-amd64/qsub
 
 cd /u/scratch/d/dechavez/SA.VCF/Filtered/20200530
 
-for line in $(cat /u/home/d/dechavez/project-rwayne/2nd.paper/4-Demography/ROH/list.sp.ROH.Het.txt); do (echo $line && \
-for i in {01..38}; do (${QSUB} -N OnlyPassVCF $SCRIPT_DIR/run_get_polymorphic_SA.canids.sh ${line} ${i});done);done
+#for line in $(cat /u/home/d/dechavez/project-rwayne/2nd.paper/4-Demography/ROH/list.sp.ROH.Het.txt); do (echo $line && \
+#for i in {01..38}; do (${QSUB} -N OnlyPassVCF $SCRIPT_DIR/run_get_polymorphic_SA.canids.sh ${line} ${i});done);done
 
 ## for i in {01..38}; do
 ## ${QSUB} -N OnlyPassVCF $SCRIPT_DIR/run_get_polymorphic_SA.canids.sh bcbr05 $i;done
@@ -32,8 +32,8 @@ for i in {01..38}; do (${QSUB} -N OnlyPassVCF $SCRIPT_DIR/run_get_polymorphic_SA
 ## for i in {01..38}; do
 ## ${QSUB} -N OnlyPassVCF $SCRIPT_DIR/run_get_polymorphic_SA.canids.sh Lve01 $i;done
 
-## for i in {01..38}; do
-## ${QSUB} -N OnlyPassVCF $SCRIPT_DIR/run_get_polymorphic_SA.canids.sh Lculp01 $i;done
+for i in {01..38}; do
+${QSUB} -N OnlyPassVCF $SCRIPT_DIR/run_get_polymorphic_SA.canids.sh Lculp01 $i;done
 
 ## for i in {01..38}; do
 ## ${QSUB} -N OnlyPassVCF $SCRIPT_DIR/run_get_polymorphic_SA.canids.sh SV16082018 $i;done
