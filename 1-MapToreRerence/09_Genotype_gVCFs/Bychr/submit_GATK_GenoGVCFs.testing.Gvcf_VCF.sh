c65e09e354e3674154cd3ab9813374a7d70b7d7f
dechavezv <dechavezv@ucla.edu>
2020-06-11 22:01:36 -0700

    testing joint VCF and gVCF

---
+++ 1-MapToreRerence/09_Genotype_gVCFs/Bychr/submit_GATK_GenoGVCFs.testing.Gvcf_VCF.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#$ -wd /u/scratch/d/dechavez/Testing
+#$ -l highp,h_data=10G,h_rt=24:00:00,h_vmem=30G,arch=intel*
+#$ -t 38-38:1
+#$ -N GTgVCF
+#$ -o /u/scratch/d/dechavez/Testing/log/
+#$ -e /u/scratch/d/dechavez/Testing/log/
+#$ -m abe
+#$ -M dechavezv
+
+source /u/local/Modules/default/init/modules.sh
+module load java
+
+java -jar -Xmx10g /u/local/apps/gatk/3.7/GenomeAnalysisTK.jar \
+-T GenotypeGVCFs \
+-R /u/home/d/dechavez/project-rwayne/canfam31/canfam31.fa \
+-allSites \
+-L chr$(printf "%02d" "$SGE_TASK_ID") \
+-V /u/scratch/d/dechavez/Testing/Lgy01_chr38.g.vcf.gz
+-V /u/scratch/d/dechavez/Testing/Lve01_chr38.vcf.gz
+-o /u/scratch/d/dechavez/Testing/testing.Lgy.Lve.vcf.gz
