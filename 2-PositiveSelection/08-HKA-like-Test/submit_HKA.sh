f1af5a9e1517368af02b606babeb5963d5586db6
dechavezv <dechavezv@ucla.edu>
2020-06-19 16:28:59 -0700

    same number of wind and steps givin error also has to be chr01 no 01

--- 2-PositiveSelection/08-HKA-like-Test/submit_HKA.sh
+++ 2-PositiveSelection/08-HKA-like-Test/submit_HKA.sh
@@ -2,9 +2,9 @@
 
 #$ -wd /u/home/d/dechavez/project-rwayne/Lvet
 #$ -l highp,h_rt=14:00:00,h_data=1G
-#$ -N subbamfltr
-#$ -o /u/home/d/dechavez/project-rwayne/QB3.SA.WolfHeav/log/reportsfilter.out
-#$ -e /u/home/d/dechavez/project-rwayne/QB3.SA.WolfHeav/log/reportsfilter.err
+#$ -N runHKA
+#$ -o /u/scratch/d/dechavez/HKA/log/
+#$ -e /u/scratch/d/dechavez/HKA/log/
 #$ -m abe
 #$ -M dechavezv
 
@@ -18,4 +18,4 @@ cd /u/scratch/d/dechavez/HKA
 
 for i in {01..38} X; do ( echo chr$i && \
 python SlidWin-HKA-like-Test.py bsve_joint_chr${i}_Annot_Mask_Filter.vcf.gz \
-100000 100000 ${i}> bsve_joint_chr${i}_Annot_Mask_Filter_passingSNPs.HKA.txt);done
+100000 10000 chr${i}> bsve_joint_chr${i}_Annot_Mask_Filter_passingSNPs.HKA.txt);done
