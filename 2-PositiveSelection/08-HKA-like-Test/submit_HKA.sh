3141681ed174b1fd76170b0a1dd2e7a1438d4fd7
dechavezv <dechavezv@ucla.edu>
2020-06-20 11:55:51 -0700

    getting genes within windows

--- 2-PositiveSelection/08-HKA-like-Test/submit_HKA.sh
+++ 2-PositiveSelection/08-HKA-like-Test/submit_HKA.sh
@@ -13,6 +13,7 @@ i=$(printf "%02d" "$SGE_TASK_ID")
 #i=X
 
 SCRIPT_DIR=/u/home/d/dechavez/project-rwayne/2nd.paper/2-PositiveSelection/08-HKA-like-Test
+OrtoBed=/u/home/d/dechavez/project-rwayne/Besd_Files/Canis_familiaris_all.bed
 
 source /u/local/Modules/default/init/modules.sh
 module load java
@@ -20,6 +21,11 @@ module load python/2.7
 
 cd /u/scratch/d/dechavez/HKA
 
-
+echo '********** Conducting HKA per window ***********' 
 python ${SCRIPT_DIR}/SlidWin-HKA-like-Test.py bsve_joint_chr${i}_TrimAlt_Annot_Mask_Filter.vcf.gz \
 100000 10000 chr${i} > bsve_joint_chr${i}_TrimAlt_Annot_Mask_Filter.HKA.txt
+echo '********** Done calculating HKA-like test ***********'
+
+echo '********** Extracting genes within windows ***********'
+python ${SCRIPT_DIR}/Genes_within_HKA.py bsve_joint_chr${i}_TrimAlt_Annot_Mask_Filter.HKA.txt ${OrtoBed}
+echo '********** Done Extracting genes within windows ***********'
