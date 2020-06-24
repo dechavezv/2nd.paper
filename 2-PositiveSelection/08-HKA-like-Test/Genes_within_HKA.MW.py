f3fca32db8cddad5bb45c5fa9a852a92a570ad15
dechavezv <dechavezv@ucla.edu>
2020-06-24 14:23:26 -0700

    get HKA for maned wolf

---
+++ 2-PositiveSelection/08-HKA-like-Test/Genes_within_HKA.MW.py
@@ -0,0 +1,30 @@
+'''
+Author: Daniel Chavez (July_24_2017)
+
+usage:
+python Genes_within_HKA.py <HKA.txt> <Orto_genes.bed>
+'''
+
+import sys
+
+file1 = open(sys.argv[1], 'r')
+file2 = sys.argv[2]
+outfile = open('Genes_HKA_' + sys.argv[1], 'w')
+
+list2 = [line.strip() for line in open(file2,'r')] #read files as list
+
+for line in file1:
+	line=line.strip()
+	line = line.split('\t')
+	chr = str(line[0]) 
+	start = float(line[1])
+	end = float(line[2])
+	for i in range(len(list2)):
+		line2 = list2[i].split('\t')
+		if chr == line2[0] and float(line2[1]) < float(end) and float(line2[2]) > float(start):
+			#print('%s\t%s' % (line,line2[3]))					
+			outfile.write(line[0] + '\t' + line[1] + '\t' + line[2] + '\t' + line[3] + '\t' + line[4] + '\t' + line[5] + '\t' + line[6] + '\t' + line[7] + '\t' + line2[3] + '\t' + line2[7] + '\n')
+		#else:
+		#	#print('%s\t%s' % (line,'NA'))
+		#	outfile.write(line[0] + '\t' + line[1] + '\t' + line[2] + '\t' + line[3] + '\t' + line[4] + '\t' + line[5] + '\t' + line[6] + '\t' + line[7] + '\t' + 'NA' + '\n')
+outfile.close()
