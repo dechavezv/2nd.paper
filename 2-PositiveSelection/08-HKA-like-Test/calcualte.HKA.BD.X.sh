244fb2df4cfee63ce5175d3de1fac1bfd5931103
dechavezv <dechavezv@ucla.edu>
2020-06-27 13:02:19 -0700

    preparing for BD chrX

--- 2-PositiveSelection/08-HKA-like-Test/calcualte.HKA.BD.X.sh
+++ 2-PositiveSelection/08-HKA-like-Test/calcualte.HKA.BD.X.sh
@@ -17,13 +17,13 @@ source /u/local/Modules/default/init/modules.sh
 module load java
 module load python/2.7
 
-cd /u/scratch/d/dechavez/HKA/MW
+cd /u/scratch/d/dechavez/HKA/BD
 
 echo '********** Conducting HKA per window ***********' 
-python ${SCRIPT_DIR}/SlidWin-HKA-like-Test.MW.py bcbr_joint_chr${i}_TrimAlt_Annot_Mask_Filter.vcf.gz \
-100000 10000 chr${i} > bcbr_joint_chr${i}_TrimAlt_Annot_Mask_Filter.HKA.txt
+python ${SCRIPT_DIR}/SlidWin-HKA-like-Test.BD.py bsve_joint_chr${i}_TrimAlt_Annot_Mask_Filter.vcf.gz \
+100000 10000 chr${i} > bsve_joint_chr${i}_TrimAlt_Annot_Mask_Filter.HKA.txt
 echo '********** Done calculating HKA-like test ***********'
 
 echo '********** Extracting genes within windows ***********'
-python ${SCRIPT_DIR}/Genes_within_HKA.MW.py bcbr_joint_chr${i}_TrimAlt_Annot_Mask_Filter.HKA.txt ${OrtoBed}
+python ${SCRIPT_DIR}/Genes_within_HKA.BD.py bsve_joint_chr${i}_TrimAlt_Annot_Mask_Filter.HKA.txt ${OrtoBed}
 echo '********** Done Extracting genes within windows ***********'
