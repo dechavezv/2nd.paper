8ce799597d094995974b89996a8923d0319198ff
dechavezv <dechavezv@ucla.edu>
2020-07-26 11:14:26 -0700

    filter rail

--- 1-MapToreRerence/06_RemoveBadReads/run_RemoveBadReads_rails.sh
+++ 1-MapToreRerence/06_RemoveBadReads/run_RemoveBadReads_rails.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-#$ -wd /u/home/d/dechavez/project-rwayne/rails.project/reads/Daniel.data.2020
+#$ -wd /u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam
 #$ -l highp,highmem,h_rt=18:00:00,h_data=20G,h_vmem=80G,arch=intel*
-#$ -o /u/home/d/dechavez/project-rwayne/rails.project/reads/Daniel.data.2020/log/rmBad.out
-#$ -e /u/home/d/dechavez/project-rwayne/rails.project/reads/Daniel.data.2020/log/rmBad.err
+#$ -o /u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam/log/rmBad.out
+#$ -e /u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam/log/rmBad.err
 #$ -m abe
 #$ -M dechavezv
 
@@ -11,8 +11,8 @@ source /u/local/Modules/default/init/modules.sh
 module load samtools
 
 ## SAMTOOLS=/u/home/j/jarobins/project-rwayne/utils/programs/samtools-1.3.1/samtools
-IN_DIR=/u/home/d/dechavez/project-rwayne/rails.project/reads/Daniel.data.2020
-OUT_DIR=/u/home/d/dechavez/project-rwayne/rails.project/reads/Daniel.data.2020
+IN_DIR=/u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam
+OUT_DIR=/u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam
 
 samtools view -hb -f 2 -F 256 -q 30 ${IN_DIR}/${1} | samtools view -hb -F 1024 > ${OUT_DIR}/${1%.bam}_Filtered.bam
 
@@ -21,6 +21,6 @@ module load java
 
 PICARD=/u/local/apps/picard-tools/current/picard.jar
 
-java -jar -Xmx20g -Djava.io.tmpdir=/u/scratch/d/dechavez/rails.project/bams/Daniel.2020/temp ${PICARD} BuildBamIndex \
-VALIDATION_STRINGENCY=LENIENT TMP_DIR=/u/scratch/d/dechavez/rails.project/bams/Daniel.2020/temp \
+java -jar -Xmx20g -Djava.io.tmpdir=/u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam/temp ${PICARD} BuildBamIndex \
+VALIDATION_STRINGENCY=LENIENT TMP_DIR=/u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam/temp \
 I=${OUT_DIR}/${1%.bam}_Filtered.bam
--- 1-MapToreRerence/06_RemoveBadReads/submit_run_RemoveBadReads_rails.sh
+++ 1-MapToreRerence/06_RemoveBadReads/submit_run_RemoveBadReads_rails.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
-#$ -wd /u/home/d/dechavez/project-rwayne/rails.project/reads/Daniel.data.2020
+#$ -wd /u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam
 #$ -l highp,h_rt=22:00:00,h_data=1G
 #$ -N subbamfltr
-#$ -o /u/home/d/dechavez/project-rwayne/rails.project/reads/Daniel.data.2020/log/reports
-#$ -e /u/home/d/dechavez/project-rwayne/rails.project/reads/Daniel.data.2020/log/reports
+#$ -o /u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam/log/reports
+#$ -e /u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam/log/reports
 #$ -m abe
 #$ -M dechavezv
 
 SCRIPT_DIR=/u/home/d/dechavez/project-rwayne/2nd.paper/1-MapToreRerence/06_RemoveBadReads
 QSUB=/u/systems/UGE8.6.4/bin/lx-amd64/qsub
 
-cd /u/home/d/dechavez/project-rwayne/rails.project/reads/Daniel.data.2020
+cd /u/scratch/d/dechavez/rails.project/bams/Daniel.2020/bam
 
 for file in *Aligned.AtlChr.MarkDup.bam; do
 	${QSUB} -N bamfiltr${file} $SCRIPT_DIR/run_RemoveBadReads_rails.sh ${file}
