11ff845102077aed14d5803df5268247ed4f8444
dechavezv <dechavezv@ucla.edu>
2020-08-02 11:23:00 -0700

    run just step 1

--- 4-Demography/Heterozygosity/TotalHete/run_HetPerInd_SA.sh
+++ 4-Demography/Heterozygosity/TotalHete/run_HetPerInd_SA.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-#$ -wd /u/scratch/d/dechavez/SA.VCF/Filtered/20200530
-#$ -l h_rt=24:00:00,h_data=2G,arch=intel*
+#$ -wd /u/home/d/dechavez/project-rwayne/SA.VCF/Combined
+#$ -l highp,h_rt=24:00:00,h_data=1G,arch=intel*
 #$ -N SAtotHet
-#$ -o /u/scratch/d/dechavez/SA.VCF/log/
-#$ -e /u/scratch/d/dechavez/SA.VCF/log/
+#$ -o /u/home/d/dechavez/project-rwayne/SA.VCF/Combined/log/
+#$ -e /u/home/d/dechavez/project-rwayne/SA.VCF/Combined/log/
 #$ -m abe
 #$ -M dechavezv
 #$ -t 1-38:1
@@ -16,12 +16,19 @@ module load python
 
 SCRIPTDIR=/u/home/d/dechavez/project-rwayne/2nd.paper/4-Demography/Heterozygosity/TotalHete
 
-Direc=/u/scratch/d/dechavez/SA.VCF/Filtered/20200530
+Direc=/u/home/d/dechavez/project-rwayne/SA.VCF/Combined
 
 cd ${Direc}
 
+i=$(printf %02d $SGE_TASK_ID)
+
+python ${SCRIPTDIR}/HetPerInd_SA.py SA_chr${i}_TrimAlt_Annot_Mask_Filter.vcf.gz ${i}
+
+#for i in {01..8}; do (python ${SCRIPTDIR}/HetPerInd_SA.py Reheader_${i}_FastqToSam.bam_Aligned.MarkDup_Filtered_Masked.vcf.gz);done
 
 #for line in $(cat /u/home/d/dechavez/project-rwayne/2nd.paper/4-Demography/ROH/list.sp.ROH.Het.txt); do /
 #python ${SCRIPTDIR}/HetPerInd_SA.py $(ls ${line}_chr$(printf %02d $SGE_TASK_ID)*vcf.gz) ${SGE_TASK_ID};done
 
-python ${SCRIPTDIR}/HetPerInd_SA.py $(ls Lculp01_chr$(printf %02d $SGE_TASK_ID)*vcf.gz) ${SGE_TASK_ID}
+## python ${SCRIPTDIR}/HetPerInd_SA.py $(ls LS01_chr$(printf %02d $SGE_TASK_ID)*.vcf.gz) ${SGE_TASK_ID}                               
+python ${SCRIPTDIR}/HetPerInd_SA.py Reheader_$(printf %02d $SGE_TASK_ID)_FastqToSam.bam_Aligned.MarkDup_Filtered_Masked.vcf.gz
+
--- 4-Demography/ROH/submit_ROH_Step1-2.sh
+++ 4-Demography/ROH/submit_ROH_Step1-2.sh
@@ -1,43 +1,39 @@
 #! /bin/bash
 
-#$ -wd  /u/scratch/d/dechavez/SA.VCF/Filtered/20200530
+#$ -wd  /u/home/d/dechavez/project-rwayne/SA.VCF/Combined
 #$ -l highp,h_rt=4:00:00,h_data=1G
 #$ -N subROH
-#$ -o /u/scratch/d/dechavez/SA.VCF/log/
-#$ -e /u/scratch/d/dechavez/SA.VCF/log/
+#$ -o /u/home/d/dechavez/project-rwayne/SA.VCF/Combined/log/
+#$ -e /u/home/d/dechavez/project-rwayne/SA.VCF/Combined/log/
 #$ -m abe
 #$ -M dechavezv
-#$ -t 1-10:1
+### #$ -t 1-10:1
 
 SCRIPT_DIR=/u/home/d/dechavez/project-rwayne/2nd.paper/4-Demography/ROH
 QSUB=/u/systems/UGE8.6.4/bin/lx-amd64/qsub
 
-cd /u/scratch/d/dechavez/SA.VCF/Filtered/20200530
+cd /u/home/d/dechavez/project-rwayne/SA.VCF/Combined
 
 #Provide a list with name of samples
 
-#for line in $(cat ${SCRIPT_DIR}/list.sp.ROH.Het.txt); do
-## ${QSUB} -N ROHstep1 $SCRIPT_DIR/Step1_ROH_VCFtoPLINK.sh ${line}
-#done
-
-### ${QSUB} -N ROHstep1 $SCRIPT_DIR/Step1_ROH_VCFtoPLINK.sh Lculp01
+for line in $(cat ${SCRIPT_DIR}/list.sp.ROH.Het.txt); do
+${QSUB} -N ROHstep1 $SCRIPT_DIR/Step1_ROH_VCFtoPLINK.sh ${line}
+done
 
 #sleep 25m
 
-cd plinkInputFiles
+#cd plinkInputFiles
 
 # For making temp.txt
 ## for a in 50 100 200; \
-## do for b in 50 100; \
-## do for c in 1000 2000 5000; \
-## do for d in 50 100 200; \
-## do for e in 1 2 3 4 5 10 20; \
-## do for f in 1 2 3 4 5 10 20; \
-## do for g in .02 .05 .1; \
-## do echo $a $b $c $d $e $f $g >> temp.txt ; done; done; done; done; done; done; done
-
-${QSUB} -N ROHstep2 ${SCRIPT_DIR}/Step2_run_PLINK_plot.sh Lculp01 $(head -n ${SGE_TASK_ID} temp.txt | tail -n 1)
+# do for b in 50 100; \
+# do for c in 1000 2000 5000; \
+# do for d in 50 100 200; \
+# do for e in 1 2 3 4 5 10 20; \
+# do for f in 1 2 3 4 5 10 20; \
+# do for g in .02 .05 .1; \
+# do echo $a $b $c $d $e $f $g >> temp.txt ; done; done; done; done; done; done; done
  
-## for line in $(cat ${SCRIPT_DIR}/list.sp.ROH.Het.txt); do
+#for line in $(cat ${SCRIPT_DIR}/list.sp.ROH.Het.txt); do
 ### ${QSUB} -N ROHstep2 ${SCRIPT_DIR}/Step2_run_PLINK_plot.sh ${line} $(head -n ${SGE_TASK_ID} temp.txt | tail -n 1)
-## done
+#done
