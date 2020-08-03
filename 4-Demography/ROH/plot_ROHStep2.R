bdb351b158e84baa2e838739143f05d8c9e87d8c
dechavezv <dechavezv@ucla.edu>
2020-08-03 14:58:50 -0700

    fix labels and colors

--- 4-Demography/ROH/plot_ROHStep2.R
+++ 4-Demography/ROH/plot_ROHStep2.R
@@ -20,7 +20,6 @@ homAs=as.numeric(gts[,(3*nn+1):(3*nn+nn)])
 hets=as.numeric(gts[,(4*nn+1):(4*nn+nn)])
 autohet=hets/calls
 
-
 #####
 
 # ROH data
@@ -63,11 +62,13 @@ for (i in 1:length(samplename)){
 
 samps=c("Cb17082018","Cth","Cbr370","Cbr383",
         "Cbr404", "Cbr388", "CDKPEI14051", "Lculp", "Lgy",
-        "Lvet", "SV16082018", "Sve313", "Sve315", "Sve338")
+        "Lvet", "SV16082018", "Sve313", "Sve315", "Sve338",
+	"AMI","DGR","DFU","DSE")
 
-mylabels=c("MW Capt.","Crab-eat fox","MW 370", "MW 383",
-           "MW 388", "MW 404", "Ethiopian", "Andean fox", "Pampas fox",
-           "Hoary fox", "BD Capt", "BD 313", "BD 315", "BD 338")
+#Orde has to be the same as in hete.txt files
+mylabels=c("Ethiopian","short-Eared-dog","BD Capt","Darwin fox","BD 315",
+	"BD 338","BD 313","Sechuran fox","MW 404","MW 388","MW 383","MW 370","MW Capt.",
+	"SA Gray fox","Andean fox","Crab-eat fox","Pampas fox","Hoary fox")
 
 MWnames=c("Cb17082018","Cbr370","Cbr383","Cbr388","Cbr404")
 BDnames=c("Sve313","Sve315","Sve338","SV16082018")
@@ -83,6 +84,10 @@ mygroups[which(samps %in% "Lgy")]=4
 mygroups[which(samps %in% "Lculp")]=5
 mygroups[which(samps %in% MWnames)]=6
 mygroups[which(samps %in% BDnames)]=7
+mygroups[which(samps %in% "AMI")]=8
+mygroups[which(samps %in% "DGR")]=9
+mygroups[which(samps %in% "DFU")]=10
+mygroups[which(samps %in% "DSE")]=11
 
 mydf=data.frame(samplename, nocalls, calls, homRs, homAs, hets, autohet, shortnums, mednums, longnums, shortsums, medsums, longsums)
 
@@ -123,13 +128,13 @@ dev.off()
 myred="#ef8a62"
 myblue="#67a9cf"
 mygrey="#bababa"
-bgcols=c(mygrey,mygrey,mygrey,mygrey,mygrey,myblue,myred)
+bgcols=c(mygrey,mygrey,mygrey,mygrey,mygrey,myblue,myred,mygrey,mygrey,mygrey,mygrey)
 
 # cols
 myred2="#67001f"
 myblue2="#053061"
 mygrey2="#1a1a1a"
-linecols=c(mygrey2,mygrey2,mygrey2,mygrey2,mygrey2,myblue2,myred2)
+linecols=c(mygrey2,mygrey2,mygrey2,mygrey2,mygrey2,myblue2,myred2,mygrey,mygrey,mygrey,mygrey)
 
 
 pdf(paste("ROH_nums_20200530_", args[1], ".pdf", sep=""), width=6.83, height=2.75, pointsize=12)
@@ -143,20 +148,25 @@ mygroups[which(samps %in% "Lgy")]=4
 mygroups[which(samps %in% "Lculp")]=5
 mygroups[which(samps %in% MWnames)]=6
 mygroups[which(samps %in% BDnames)]=7
+mygroups[which(samps %in% "AMI")]=8
+mygroups[which(samps %in% "DGR")]=9
+mygroups[which(samps %in% "DFU")]=10
+mygroups[which(samps %in% "DSE")]=11
+
 
 ymax=1.2*max(mydf2$shortnums)
 plot(mydf2$mygroups,mydf2$shortnums, bty="n", ylim=c(0,ymax), axes=F, ylab="Total Number of ROH", main="Short ROH\n[100kb, 1Mb)", xlab="", cex=1.5, pch=21, col=linecols[mydf2$mygroups], bg=bgcols[mydf2$mygroups], font.main=1, cex.main=1, xlim=c(1,8))
 axis(side=2, line=.5)
-axis(side=1, labels=c("Ethiopia","Crab-eating fox","Hoary fox","Pampas fox", "Andean fox", "Maned wolf", "Bush dog"), at=seq(1,7), las=2)
+axis(side=1, labels=c("Ethiopia","Crab-eating fox","Hoary fox","Pampas fox", "Andean fox", "Maned wolf", "Bush dog","short-Eared-dog","SA Gray fox","Darwin fox", "Sechuran fox"), at=seq(1,11), las=2)
 
 ymax=1.2*max(mydf2$mednums)
 plot(mydf2$mygroups,mydf2$mednums, bty="n", ylim=c(0,ymax), axes=F, ylab="", main="Medium ROH\n[1Mb, 10Mb)", xlab="", cex=1.5, pch=21, col=linecols[mydf2$mygroups], bg=bgcols[mydf2$mygroups], font.main=1, cex.main=1, xlim=c(1,8))
 axis(side=2, line=.5)
-axis(side=1, labels=c("Ethiopia","Crab-eating fox","Hoary fox","Pampas fox", "Andean fox", "Maned wolf", "Bush dog"), at=seq(1,7), las=2)
+axis(side=1, labels=c("Ethiopia","Crab-eating fox","Hoary fox","Pampas fox", "Andean fox", "Maned wolf", "Bush dog", "short-Eared-dog","SA Gray fox","Darwin fox", "Sechuran fox"), at=seq(1,11), las=2)
 
 ymax=1.2*max(mydf2$longnums)
 plot(mydf2$mygroups,mydf2$longnums, bty="n", ylim=c(0,ymax), axes=F, ylab="", main="Long ROH\n[10Mb,100Mb)", xlab="", cex=1.5, pch=21, col=linecols[mydf2$mygroups], bg=bgcols[mydf2$mygroups], font.main=1, cex.main=1, xlim=c(1,8))
 axis(side=2, line=.5)
-axis(side=1, labels=c("Ethiopia","Crab-eating fox","Hoary fox","Pampas fox", "Andean fox", "Maned wolf", "Bush dog"), at=seq(1,7), las=2)
+axis(side=1, labels=c("Ethiopia","Crab-eating fox","Hoary fox","Pampas fox", "Andean fox", "Maned wolf", "Bush dog" , "short-Eared-dog","SA Gray fox","Darwin fox", "Sechuran fox"), at=seq(1,11), las=2)
 
 dev.off()
