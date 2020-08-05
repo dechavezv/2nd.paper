b9bd3ed8a2bc924ba57041a22b6917a91c0649ab
dechavezv <dechavezv@ucla.edu>
2020-08-05 16:08:35 -0700

    get only pass from rails scaffolds

--- 4-Demography/PCA/run_get_polymorphic_rails.sh
+++ 4-Demography/PCA/run_get_polymorphic_rails.sh
@@ -1,21 +1,39 @@
 #! /bin/bash
 #$ -wd /u/scratch/d/dechavez/rails.project/VCF/Indv/Scaf
-#$ -l h_rt=60:00:00,h_data=1G,highp
+#$ -l h_rt=24:00:00,h_data=1G,h_vmem=5G
 #$ -N OnlySNPs
-#$ -o /u/scratch/d/dechavez/rails.project/VCF/Indv/Scaf/VCF/log/
+#$ -o /u/scratch/d/dechavez/rails.project/VCF/Indv/Scaf/log/
 #$ -e /u/scratch/d/dechavez/rails.project/VCF/Indv/Scaf/log/
 #$ -m abe
 #$ -M dechavezv
+#$ -t 8-8:1
+
+i=$(printf "%02d" "$SGE_TASK_ID")
 
 source /u/local/Modules/default/init/modules.sh
 module load python
 
 cd /u/scratch/d/dechavez/rails.project/VCF/Indv/Scaf
 
-cat Reheader_allchr_FastqToSam.bam_Aligned.MarkDup_Filtered_Masked.vcf \
-grep -v "^#" | grep -v "FAIL" | grep -v "WARN" | grep -v "NO_VARIATION" | grep -v "NON_REF" | grep -vE '\./\.' | \
-grep -v "AF=0.0;" | grep -v "AF=0.00;" | grep -v "AF=1.0" > /u/home/d/dechavez/project-rwayne/rails.project/VCF/DanielData/LS_joint_allchr_Annot_Mask_Filter_passingSNPs.scaf.vcf
+zcat Reheader_${i}_FastqToSam.bam_Aligned.MarkDup_Filtered_Masked.vcf.gz \
+grep -v "^#" | grep -v "FAIL" | grep -v "WARN" | grep -vE '\./\.' | /u/home/d/dechavez/tabix-0.2.6/bgzip -c \
+> onlyPass/Reheader_${i}_FastqToSam.bam_Aligned.MarkDup_Filter_passingSNPs.vcf.gz
 
+vcf=Reheader_${i}_FastqToSam.bam_Aligned.MarkDup_Filter_passingSNPs.vcf.gz
+
+cd onlyPass
+
+#/u/home/d/dechavez/tabix-0.2.6/bgzip -c ${vcf} > ${vcf}.gz
+/u/home/d/dechavez/tabix-0.2.6/tabix -p vcf ${vcf}
+
+#rm ${PREFIX}_chr${i}_Annot_Mask_Filter_passingSNPs.vcf
+
+
+# polymorphic
+#grep -v "^#" | grep -v "FAIL" | grep -v "WARN" | grep -v "NO_VARIATION" | grep -v "NON_REF" | \
+#grep '1/1\|0/1' > /u/home/d/dechavez/project-rwayne/rails.project/VCF/DanielData/LS_joint_allchr_Annot_Mask_Filter_passingSNPs.Scaf.vcf
+
+#Good Quality
 # grep -v "^#" | grep -v "FAIL" | grep -v "WARN" | grep -vE '\./\.' \
 # > /u/home/d/dechavez/project-rwayne/rails.project/VCF/DanielData/msmc/LS_joint_chr$(printf $SGE_TASK_ID)_Annot_Mask_Filter_passingSNPs.vcf
 
